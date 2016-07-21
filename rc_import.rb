20cbbd37e7acf21f0064f5cfa395d3a976d41a45
Matthew Sullivan <hayduke19us@gmail.com>
2016-07-21 14:15:07 -0500

    clean up and organize error messages

--- rc_import.rb
+++ rc_import.rb
@@ -1,42 +1,96 @@
-#!/usr/bin/env script/rails runner
-
-require 'optparse'
 require 'csv'
 
-options = {}
-
-OptionParser.new do |opts|
-  opts.banner = "Usage: ruby rc_import.rb [options] 'file_path' file_path' 'file_path'"
-
-  opts.on("-v", "--verbose", "Run verbosely") do
-    options[:verbose] = true
-  end
-end.parse!
-
-VERBOSE = options[:verbose]
-START   = Time.now.to_f
-
-def counter_msg
-  print '.'
-end
-
-def failure_msg
-  print "F\n\n"
-end
-
+START = Time.now.to_f
 
 def total_time
   Time.now.to_f - START
 end
 
-def profile_msg
-  %(
-     Finished in #{total_time} seconds.
-   )
+module ErrorHandler
+  def handle(error)
+    raise "\n\n#{error}\n\n"
+  end
 end
+
+module ErrorMessages
+  def missing_files_msg
+    %(
+      No file paths were given as an argument.
+    )
+  end
+
+  def invalid_csv_headers_msg(valid_headers)
+    %(
+      The CSV file is missing the correct headers. Must be one of #{valid_headers}
+    )
+  end
+
+  def supplier_property_not_found_msg(room_category)
+    %(
+      Supplier property not found
+      - supplier: #{room_category.supplier_code}
+      - property_code: #{room_category.property_id}
+    )
+  end
+
+  def not_equal(type, mapping_attribute, category_attribute)
+    %(
+      #{type.capitalize} conflict
+        - room type mapping: #{mapping_attribute}
+        - room category: #{category_attribute}
+    )
+  end
+
+  def room_category_has_en_name_translation_msg
+    %(
+      Room category already has an english translation
+    )
+  end
+end
+
+module SuccessMessages
+  def mapping_files_msg(files)
+    %(
+       You are about to map #{files.count} Files
+       Files: #{files.join(", ")}
+     )
+  end
+
+  def profile_msg
+    %(
+       Finished in #{total_time} seconds.
+     )
+  end
+end
+
+
+module Messages
+  include ErrorHandler
+  include ErrorMessages
+  include SuccessMessages
+
+  def say(msg)
+    puts space msg
+  end
+
+  def space(msg)
+    "\n\n#{msg}\n\n"
+  end
+
+  def counter_msg
+    print '.'
+  end
+
+  def failure_msg
+    print "F\n\n"
+  end
+end
+
 
 module Parser
   class RoomCategoryImport
+    include Messages
+
     attr_reader :files, :rows
 
     def initialize(args)
@@ -45,10 +99,14 @@ module Parser
     end
 
     def parse
-      puts verbose_msg_missing_files if VERBOSE && files.empty?
-      puts verbose_msg_files if VERBOSE && files.any?
+      if files.empty?
+        say missing_files_msg
+      else
+        say mapping_files_msg files
+      end
+
       while files.any?
-        counter_msg  if VERBOSE
+        counter_msg
 
         ARGV.replace [files.shift]
         rows.push CSV.new(ARGF.read, headers: true, header_converters: :symbol, converters: :all).to_a.map(&:to_hash)
@@ -56,41 +114,30 @@ module Parser
 
       rows
     end
-
-    def verbose_msg_files
-      %(
-         You are about to map #{files.count} Files
-         Files: #{files.join(", ")}
-       )
-    end
-
-    def verbose_msg_missing_files
-      %(
-         No file paths were given as an argument.
-         Type `ruby rc_import.rb -h` for Help.
-       )
-    end
   end
 end
 
 module Builder
   class RoomCategories
+    include Messages
     attr_reader :rows
+    attr_accessor :category, :mapping
 
     def initialize(args)
       @rows = args.fetch(:rows, [])
     end
 
-    def to_room_categories
+    def rows_to_templates
       rows.map do |row|
         sanitized_row = sanitize_row(row.first)
 
         if sanitized_row.any?
-          RoomCategory.new(sanitized_row)
+          RoomCategoryTemplate.new(sanitized_row)
         else
-          failure_msg if VERBOSE
-          puts profile_msg if VERBOSE
-          raise "\n\nThe CSV file is missing the correct headers. Must be one of #{sanitized_keys.join(", ")}\n\n"
+          failure_msg
+          say profile_msg
+
+          handle invalid_csv_headers_msg(sanitized_keys.join(', '))
         end
       end
     end
@@ -104,49 +151,55 @@ module Builder
     end
 
     def build
-      to_room_categories.each do |room_category|
-        unless room_category.supplier_property.present?
-          raise "\nSupplier property not found supplier: #{room_category.supplier_code}" \
-                "property_code: #{room_category.property_id}"
+      rows_to_templates.each do |template|
+        if template.supplier_property.present?
+          build_room_category template
+          build_room_type_mapping template
+        else
+          handle supplier_property_not_found_msg template
         end
 
-        rc = room_category.property.room_categories.find_or_initalize_by id: room_category.room_code
-
-        rc_mapping = rc.room_type_mappings.find_or_initialize_by(
-          room_type_id: room_category.room_type_uuid ,
-          supplier_property_id: room_category.supplier_property.id
-        )
-
-        unless room_type_id_set? rc_mapping, rc.room_type_uuid
-          raise conflict_error_msg 'room_type_id', rc_mapping, rc.room_type_uuid
-        end
-
-        unless room_code_conflict? rc_mapping, rc.room_code
-          raise conflict_error_msg 'room_type_code', rc_mapping, rc.room_type_uuid
-        end
-
-        rc_mapping.supplier_property = rc.supplier_property
-        rc_mapping.room_type_code    = rc.room_type_code
-        rc_mapping.room_type_id      = rc.room_type_id
-
-        rc.name.build({ language: 'en', text: rc.name }, UserTranslatedText)
-        rc.save!
+        category.save!
       end
 
-      puts profile_msg if VERBOSE
+      say profile_msg
     end
 
-    def room_type_id_set?(mapping, uuid)
-      mapping.room_type_id.presence && mapping.room_type_id != uuid
+    def build_room_category(template)
+      self.category = template.property.room_categories.find_or_initalize_by id: template.room_code
+
+      if category.en_name_text
+        say room_category_has_en_translation_msg
+      else
+        category.name.build({ language: 'en', text: template.name }, UserTranslatedText)
+      end
     end
 
-    def conflict_error_msg(type, mapping, rc)
-      "\nFound room #{type} that differs - mapping: #{mapping.room_type_code} / category: #{rc.room_code}\n"
+    def build_room_type_mapping(template)
+      self.mapping = room_category.room_type_mappings.find_or_initialize_by(
+        room_type_id: room_category.room_type_uuid,
+        supplier_property_id: room_category.supplier_property.id
+      )
+
+      if not_equal? mapping.room_type_id, template.room_type_uuid
+        handle not_equal_msg 'room_type_id', mapping.room_type_id, category_template.room_type_uuid
+      end
+
+      if not_equal? mapping_template.room_type_code, template.room_code
+        handle not_equal_msg 'room_type_code', mapping.room_type_code, template.room_code
+      end
+
+      mapping.supplier_property = template.supplier_property
+      mapping.room_type_code    = template.room_code
+      mapping.room_type_id      = template.room_type_uuid
     end
 
+    def not_equal?(c1, c2)
+      c1.presence && c1 != c2
+    end
   end
 
-  class RoomCategory
+  class RoomCategoryTemplate
     attr_reader :brand_id, :room_code, :property_id, :supplier_code, :subsupplier_code
 
     def initialize(args)
